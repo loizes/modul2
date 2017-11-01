b12d5466ed34e21c5bdda0763e29d35121dbb7b2
loizes <33106820+loizes@users.noreply.github.com>
2017-11-01 12:54:53 +0300

    1

--- assets/js/createfields.js
+++ assets/js/createfields.js
@@ -1,4 +1,6 @@
 $().ready(function(){
+    
+    $(".cdsniga").hide();
     $(".selection").change(function(){
         if($(this).val()==0){
             $(".cdsniga").hide(150);
@@ -7,7 +9,7 @@ $().ready(function(){
             $(".cdsniga").hide(150);
         }
         else{
-            $(".cdsniga").show(150);
+            $(".cdsniga").show(1500);
         }
     })
 })
